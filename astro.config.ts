a2d1cbce5befd5c41e92222a36bb91ed5bc76960
Absurdities Media LLC <absurditiesmediallc@gmail.com>
2025-04-29 01:59:02 -0400

    Add google analytics

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
 import { SITE } from "./src/config";
 
@@ -16,6 +17,11 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
     mdx(),
   ],
   markdown: {
